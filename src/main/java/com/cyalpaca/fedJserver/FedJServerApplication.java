c3deacbe1e4330cf4f77735401bf4fa360830d70
Cyalpaca <32167389+Cyalpaca@users.noreply.github.com>
2023-10-24 17:39:05 +0800

    Init Spring Boot project structure.

---
+++ src/main/java/com/cyalpaca/fedJserver/FedJServerApplication.java
@@ -0,0 +1,13 @@
+package com.cyalpaca.fedJserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedJServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FedJServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyalpaca/fedJserver/FedJServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyalpaca.fedJserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedJServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
